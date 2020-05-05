7b4b414f1d0b3c4cee7d644d013b8c5c1c8d5320
jbblanton <jessicabblanton@gmail.com>
2020-05-05 19:27:14 +0000

    Split file; Made bigrams

--- markov.py
+++ markov.py
@@ -2,6 +2,7 @@
 
 from random import choice
 
+input_path = "green-eggs.txt"
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
@@ -10,42 +11,32 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
+    contents = open(file_path).read()
 
-    return "Contents of your file as one long string"
+    return contents
+    #print(contents.split())
+
+#print(open_and_read_file(input_path))
 
 
 def make_chains(text_string):
-    """Take input text as string; return dictionary of Markov chains.
+    """Take input text as string; return dictionary of Markov chains. """
 
-    A chain will be a key that consists of a tuple of (word1, word2)
-    and the value would be a list of the word(s) that follow those two
-    words in the input text.
+    # break string into words
+    words = open_and_read_file(text_string).split()
+    print(words)
 
-    For example:
-
-        >>> chains = make_chains("hi there mary hi there juanita")
-
-    Each bigram (except the last) will be a key in chains:
-
-        >>> sorted(chains.keys())
-        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
-
-    Each item in chains is a list of all possible following words:
-
-        >>> chains[('hi', 'there')]
-        ['mary', 'juanita']
-        
-        >>> chains[('there','juanita')]
-        [None]
-    """
-
+    # break words into pairs
     chains = {}
+    
+    for i in range(len(words) - 1):
+        chains[(words[i], words[i + 1])] = None
 
-    # your code goes here
 
-    return chains
+    # return chains
+    print(chains)
 
+print(make_chains(input_path))    
 
 def make_text(chains):
     """Return text from chains."""
@@ -63,9 +54,9 @@ input_path = "green-eggs.txt"
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+##chains = make_chains(input_text)
 
 # Produce random text
-random_text = make_text(chains)
+##random_text = make_text(chains)
 
-print(random_text)
+##print(random_text)
